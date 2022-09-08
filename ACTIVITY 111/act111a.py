a00305d7086c2f8ba00147763d227cc3c049eae7
leo <somebody1337666@gmail.com>
2022-09-08 14:25:21 -0500

    checkmate mehring

--- ACTIVITY 111/act111a.py
+++ ACTIVITY 111/act111a.py
@@ -2,9 +2,16 @@ import turtle as trtl
 
 painter = trtl.Turtle()
 
-painter.turtlesize(5)
-painter.forward(100)
-painter.right(90)
+painter.turtlesize(3)
+painter.pencolor('green')
+painter.pensize(3)
+
+for i in range(2):
+    painter.forward(200)
+    painter.right(90)
+    painter.forward(100)
+    painter.right(90)
+    painter.circle(20)
  
 wn = trtl.Screen()
 wn.mainloop()
