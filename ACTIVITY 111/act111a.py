321fe3c01839b567d7ebf4604baa84553871a4a1
leo <somebody1337666@gmail.com>
2022-09-08 14:30:23 -0500

    it looks cuter now i think

--- ACTIVITY 111/act111a.py
+++ ACTIVITY 111/act111a.py
@@ -6,9 +6,7 @@ painter.turtlesize(3)
 painter.pencolor('green')
 painter.pensize(3)
 
-for i in range(2):
-    painter.forward(200)
-    painter.right(90)
+for i in range(4):
     painter.forward(100)
     painter.right(90)
     painter.circle(20)
