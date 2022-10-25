9037f4c37a8584121b9994c25b7649ae2f6b5e74
leo <somebody1337666@gmail.com>
2022-10-25 14:14:39 -0500

    i cant rn

--- ACTIVITY 122/leaderboard.py
+++ ACTIVITY 122/leaderboard.py
@@ -41,11 +41,14 @@ def get_scores(file_name):
 
     # TODO 3: use a while loop to index beyond the comma, skipping the player's name
     while (line[index] != ","):
+      print(line[index])
       index += 1
+    index += 1
 
     # TODO 4: use a while loop to get the score
     while line[index] != "\n":
       leader_score += line[index]
+      print(line)
       index += 1
     # TODO 5: add the player score to the scores list
       scores.append(int(leader_score))
@@ -63,26 +66,27 @@ def update_leaderboard(file_name, leader_names, leader_scores,  player_name, pla
   # TODO 8: loop through all the scores in the existing leaderboard list
   for score in range(len(leader_scores)):
     # TODO 9: check if this is the position to insert new score at
-    if leader_scores[score] >= player_score and player_score >= leader_scores[score + 1]:
+    if player_score >= leader_scores[score]:
       break
     else:
       index = index + 1
   
   # TODO 10: insert new player and score
-
+  leader_scores.insert(index, player_score)
+  leader_names.insert(index, player_name)
   # TODO 11: keep both lists at 5 elements only (top 5 players)
-  
+  if (len(leader_scores) > 5):
+    leader_scores.pop()
+    leader_names.pop()
   # TODO 12: store the latest leaderboard back in the file
   
-  '''
   leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
  
   # TODO 13 loop through all the leaderboard elements and write them to the the file
-  for   :
+  for index in range(len(leader_names)):
     leaderboard_file.write(leader_names[index] + "," + str(leader_scores[index]) + "\n")
 
   leaderboard_file.close()
-   '''
   
 
 # draw leaderboard and display a message to player
@@ -109,10 +113,10 @@ def draw_leaderboard(high_scorer, leader_names, leader_scores, turtle_object, pl
   turtle_object.pendown()
 
   # TODO 14: display message about player making/not making leaderboard
-  '''
+  if player_score >= leader_scores[4] or len(leader_scores) < 5:
     turtle_object.write("Congratulations!\nYou made the leaderboard!", font=font_setup)
+  else:
     turtle_object.write("Sorry!\nYou didn't make the leaderboard.\nMaybe next time!", font=font_setup)
-  '''
 
   # move turtle to a new line
   turtle_object.penup()
@@ -120,8 +124,9 @@ def draw_leaderboard(high_scorer, leader_names, leader_scores, turtle_object, pl
   turtle_object.pendown()
   
   # TODO 15: Display a gold/silver/bronze message if player earned a gold/silver/or bronze medal; display nothing if no medal
-  '''
-    turtle_object.write("You earned a gold medal!", font=font_setup)
-    turtle_object.write("You earned a silver medal!", font=font_setup)
+  if player_score >= bronze_score and player_score < silver_score:
     turtle_object.write("You earned a bronze medal!", font=font_setup)
-  '''
+  elif player_score >= silver_score and player_score < gold_score:
+    turtle_object.write("You earned a silver medal!", font=font_setup)
+  elif player_score >= gold_score:
+    turtle_object.write("You earned a gold medal!", font=font_setup)
