236bcd4e9b38500738683c7d95b0d9f17bdf9095
leo <somebody1337666@gmail.com>
2022-11-07 14:26:31 -0600

    isanity

--- ACTIVITY 123/123a.py
+++ ACTIVITY 123/123a.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -8,8 +9,8 @@ pear_image = "pear.gif"
 # game variables
 ground_height = -200
 apple_letter_x_offset = 0
-apple_letter_y_offset = 0
-letter_font = ("Arial", 50, "bold")
+apple_letter_y_offset = -40
+letter_font = ("Arial", 45, "bold")
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
@@ -23,10 +24,12 @@ apple = trtl.Turtle()
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
+  wn.tracer(False)
   global apple_image
   active_apple.shape(apple_image)
   active_apple.penup()
-  
+  draw_letter("A", active_apple)
+  wn.tracer(True)
   wn.update()
 
 def draw_pear(active_pear):
@@ -35,15 +38,31 @@ def draw_pear(active_pear):
   active_pear.penup()
   wn.update()
 
+def reset_apple(active_apple):
+  new_x = rand.randint(-150, 150)
+  new_y = rand.randint(0, 100)
+  print(new_y)
+  active_apple.goto(new_x, new_y)
+  active_apple.showturtle()
+  draw_apple(active_apple)
+
+
 def drop_apple():
   global ground_height, apple
+  wn.tracer(True)
   apple.goto(apple.xcor(), ground_height)
+  apple.clear()
+  apple.hideturtle()
+  wn.tracer(False)
+  reset_apple(apple)
 
 def draw_letter(letter, active_apple):
-  global apple_letter_x_offset, apple_letter_y_offset
+  global apple_letter_x_offset, apple_letter_y_offset, letter_font
   active_apple.color('white')
   pos = active_apple.position()
   active_apple.setpos(active_apple.xcor() + apple_letter_x_offset, active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=letter_font, align="center")
+  active_apple.setpos(pos)
 
 
 #-----function calls-----
