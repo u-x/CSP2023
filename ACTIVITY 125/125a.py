485f679a3978f369e07948ce0a83d57c1c979a14
leo <somebody1337666@gmail.com>
2022-11-30 14:29:44 -0600

    im going insane

--- ACTIVITY 125/125a.py
+++ ACTIVITY 125/125a.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random
 wn = trtl.Screen()
 
 # background
@@ -8,6 +9,7 @@ trtl.addshape("H:\\CSP2023\\ACTIVITY 125\\keke.gif")
 # vars
 p1score = 0
 p2score = 0
+ballinplay = False
 
 wn.tracer(False)
 
@@ -55,12 +57,56 @@ p2scorewriter.pu()
 p2scorewriter.color("white")
 p2scorewriter.goto(30, 220)
 p2scorewriter.write("0", align="left", font=scorefont)
+wn.tracer(True)
 
 # left player paddle
 p1paddle = trtl.Turtle()
 p1paddle.shape("H:\\CSP2023\\ACTIVITY 125\\keke.gif")
 p1paddle.penup()
 p1paddle.color("#ffffff")
-p1paddle.goto(-50, 0)
+p1paddle.goto(-365, 0)
+
+# left player paddle
+p2paddle = trtl.Turtle()
+p2paddle.shape("H:\\CSP2023\\ACTIVITY 125\\keke.gif")
+p2paddle.penup()
+p2paddle.color("#ffffff")
+p2paddle.goto(365, 0)
+
+pball = trtl.Turtle()
+pball.shape("square")
+pball.color("white")
+pball.pu()
+pball.turtlesize(1)
+pball.speed("fastest")
+
+def play_ball():
+    global ballinplay
+    pball.goto(0, 0)
+    angled = False
+    angle = 0
+    while angled == False:
+        if angle >= 0 and angle <= 45:
+            angle = random.randint(0, 360)
+        elif angle >= 315:
+            angle = random.randint(0, 360)
+        elif angle >= 135 and angle <= 225:
+            angle = random.randint(0, 360)
+        else:
+            angled = True
+    print(angle)
+    if (angled == True):
+        pball.setheading(angle)
+        ballinplay = True
+        while ballinplay == True:
+            pball.forward(3)
+            # if (pball.ycor() <= -290 or pball.ycor >= 290):
+            #     anglern = pball.heading()
+            #     if (anglern > 180):
+            #         bruhangle = anglern-90
+            #         if pball.ycor > 0:
+            #             pball.setheading(270-)
+
+play_ball()
 
 wn.mainloop()
