a1c5e1e4d59733de9a94ee57a6d312a959593a36
leo <somebody1337666@gmail.com>
2022-12-21 14:30:12 -0600

    MAN WHATTTT

--- ACTIVITY 214/214a.py
+++ ACTIVITY 214/214a.py
@@ -8,33 +8,34 @@ root.wm_geometry("400x200")
 root.wm_title("Authorization")
 
 # create empty frame
-frame_login = tk.Frame(root, width=400, height=300)
-frame_login.grid(row=0, column=0, sticky='n')
-lbl_username = tk.Label(frame_login, text='Username:', font="{Comic Sans MS}")
+frame_login = tk.Frame(root, background="white", width=400, height=300)
+frame_login.grid(row=0, column=0, sticky='news')
+lbl_username = tk.Label(frame_login, text='Username:', font="{Comic Sans MS}", background="white")
 lbl_username.pack(pady=5)
 
 ent_username = tk.Entry(frame_login, bd=3, width=30)
-ent_username.pack(pady=5)
+ent_username.pack(pady=7.5)
 
-lbl_passwd = tk.Label(frame_login, text='Password:', font="{Comic Sans MS}")
+lbl_passwd = tk.Label(frame_login, text='Password:', font="{Comic Sans MS}", background="white")
 lbl_passwd.pack(pady=5)
 
 ent_passwd = tk.Entry(frame_login, show="*", bd=3, width=30)
-ent_passwd.pack(pady=5)
+ent_passwd.pack(pady=7.5)
 
-frame_auth = tk.Frame(root)
-frame_auth.grid(row=0, column=0, sticky='n')
+frame_auth = tk.Frame(root, background="purple")
+frame_auth.grid(row=0, column=0, sticky='news')
 
 passwd_label = tk.Label(frame_auth, font="{Comic Sans MS}")
 
 def testbutton():
     frame_auth.tkraise()
     passwordlol = ent_passwd.get()
-    passwd_label.config(text=passwordlol)
-    passwd_label.pack()
+    passwd_label.config(text=passwordlol, background="purple")
+    passwd_label.pack(expand=True)
 
-btn_login = tk.Button(frame_login, text="Login", font="{Comic Sans MS}", command=testbutton)
-btn_login.pack(padx=185, pady=5)
+btn_img = tk.PhotoImage(file="./login_button.png")
+btn_login = tk.Button(frame_login, bd=0, command=testbutton, image=btn_img, background="white")
+btn_login.pack(padx=130, pady=5)
 
 frame_login.tkraise()
 
--- ACTIVITY 214/214b.py
+++ ACTIVITY 214/214b.py
@@ -7,13 +7,16 @@ root.wm_geometry("200x200")
 blue_frame = tk.Frame(root, background="#0000FF", width=125, height=100)
 blue_frame.grid(row=0, column=0, sticky="news")
 
-green_frame = tk.Frame(root, background="#00FF00", width=125, height=100)
+green_frame = tk.Frame(root, background="#00FF00", width=75, height=100)
 green_frame.grid(row=0, column=1, sticky="news")
 
 red_frame = tk.Frame(root, background="#FF0000", width=125, height=100)
 red_frame.grid(row=1, column=0, sticky="news")
 
-yellow_frame = tk.Frame(root, background="#FFFF00", width=100, height=150)
+yellow_frame = tk.Frame(root, background="#FFFF00", width=75, height=100)
 yellow_frame.grid(row=1, column=1, sticky="news")
+
+firstwidg = tk.Scale(root, orient='horizontal')
+firstwidg.grid(row=0, column=0, sticky='news')
 
 root.mainloop()
